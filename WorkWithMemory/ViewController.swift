a045e91c1d8c629ebc119ac5187569ea471fb59e
kairzhan8 <kairzhankural@gmail.com>
2020-09-17 17:53:13 +0600

    if no link from class, class will be reallocated

--- WorkWithMemory/ViewController.swift
+++ WorkWithMemory/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label.isHidden = true
+        
+        var first: MyClass? = MyClass()
+        var second = first
+        var third = first
+        
+        first = nil
+        second = nil
+        third = nil
     }
 
+    @IBAction func openSecondVC(_ sender: UIButton) {
+    }
+    
+    
+}
 
+class MyClass {
+    var someNumber = 16
+    
+    deinit {
+        print("MyClass will be dealocated")
+    }
 }
 
